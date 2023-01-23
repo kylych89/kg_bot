61204832eeed91e38179fec147dd78610613d6e8
kylych <kylycsydykov@gmail.com>
2023-01-23 09:37:48 +0600

    changed wrapper func

--- decorator.py
+++ decorator.py
@@ -2,11 +2,11 @@
 #
 #
 # def check_time(func):
-#     def wrapper(*args, **kwargs):
+#     def wrapper():
 #         start = datetime.now()
 #         print('start decor')
 #
-#         func(*args, **kwargs)
+#         func()
 #
 #         print(datetime.now() - start)
 #         print('end decor')
@@ -14,27 +14,29 @@
 #     return wrapper
 #
 #
-# @check_time
-# def first(l):
+# def first():
+#     l = []
 #     for i in range(0, 10):
 #         if i % 2 == 0:
 #             l.append(i)
 #     print(f'I am first func list {l}')
 #
 #
-# @check_time
-# def second(count):
+# ans = check_time(first)
+# ans()
+#
+#
+# def second():
 #     l = [x for x in range(0, 10) if x % 2 == 0]
-#     count = tuple(l)
-#     print(f'I am second func tuple {count}')
+#     print(f'I am second func tuple {tuple(l)}')
 #
 #
 # f = first
 # s = second
 #
-# f([1, 2, 3, 4, 5, 6, 7, 8])
-# s((1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
-
+# f()
+# print('___________________________________________________________')
+# s()
 
 #####################################################################################################################
 
@@ -91,4 +93,3 @@
 
 
 # number()
-
